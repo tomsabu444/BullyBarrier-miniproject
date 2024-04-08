8a5abca0e14f697d974dcad63771e38fa0265582
TOM SABU <tomsabu0000@gmail.com>
2024-04-08 06:14:24 +0530

    updated Routing

---
+++ client/src/pages/Sign_In.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sign_In() {
+  return (
+    <div>Sign_in</div>
+  )
+}
+
+export default Sign_In
---
+++ client/src/pages/Sign_Up.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sign_Up() {
+  return (
+    <div>Sign_Up</div>
+  )
+}
+
+export default Sign_Up
