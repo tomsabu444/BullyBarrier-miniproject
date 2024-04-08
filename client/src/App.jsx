82bf3e1754a7271bdfa62f7a81d59b8f26a20253
TOM SABU <tomsabu0000@gmail.com>
2024-04-08 20:33:36 +0530

    Authentication --progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"; //Routing
 
-
 //Pages
-import Sign_In from './pages/Sign_In';
-import Sign_Up from './pages/Sign_Up';
+import Sign_In from "./pages/Sign_In";
+import Sign_Up from "./pages/Sign_Up";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
-      <Route path="/sign-in" element={<Sign_In />} />
-      <Route path="/sign-up" element={<Sign_Up />} />
-    </Routes>
-  </BrowserRouter>
-  )
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<Sign_In />} />
+          <Route path="/sign-up" element={<Sign_Up />} />
+        </Routes>
+
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
