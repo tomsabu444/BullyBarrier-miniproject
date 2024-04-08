58a170714b049aec7fa22448b1fdffb8a147e36d
TOM SABU <tomsabu0000@gmail.com>
2024-04-08 06:44:27 +0530

    fixed cros

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cros  = require('cors');
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT ;
 
 app.use(cros());
 app.use(express.json());
 
-app.listen(port, () => console.log(`Server running on ${port}`));   
+app.listen(port, () => console.log(`Server running~!`));   
