475161e0de0b5d5e114afe37dde6fc1e71896495
TOM SABU <tomsabu0000@gmail.com>
2024-04-10 16:35:30 +0530

    User Authentication completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,23 @@
-
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { useClerk } from "@clerk/clerk-react";
 
 function Home() {
 
+  const {user } = useClerk();
 
+  console.log(user);
   return (
+    <>
+      <div>Home page
 
+<h1>Welcome {user.username}</h1>
 
-    <div>Home page</div>
-  )
+      </div>
+
+      <Navbar />
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- server/server.js
+++ server/server.js
@@ -10,3 +10,9 @@ app.use(cros());
 app.use(express.json());
 
 app.listen(port, () => console.log(`Server running~!`));   
+
+
+//database connect
+mongoose.connect(process.env.MONGODB_URL).then(()=> {
+    console.log( "MongoDB Connected!" );
+}).catch((err) => console.log(err.message));
