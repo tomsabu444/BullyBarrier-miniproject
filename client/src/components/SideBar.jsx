0bdd17d821f18ec218f2294bb008ec842066608d
TOM SABU <tomsabu0000@gmail.com>
2024-04-13 16:45:28 +0530

    SideBar --progress

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./style/SideBar.css";
+import { useClerk } from "@clerk/clerk-react";
+
+//
+import ManageAccountsSharpIcon from "@mui/icons-material/ManageAccountsSharp";
+import Divider from "@mui/material/Divider";
 
 function SideBar() {
+  const { user } = useClerk();
+
   return (
-    <div>SideBar</div>
-  )
+    <div className="sidebar">
+      <div className="sidebar-info">
+        <div className="avatar-box">
+          <img draggable="false" src={user.imageUrl} alt="user-image" />
+        </div>
+        <div className="profile-name">
+          <h2>{user.fullName}</h2>
+          <p>@{user.username}</p>
+        </div>
+        <div className="account-icon">
+          <ManageAccountsSharpIcon />
+        </div>
+      </div>
+      <Divider />
+      <h4>followers</h4>
+      <h4>following</h4>
+    </div>
+  );
 }
 
-export default SideBar
+export default SideBar;
