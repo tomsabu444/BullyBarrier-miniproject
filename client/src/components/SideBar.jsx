f85a4b26778ed97e7172932e5606799b0a29db18
TOM SABU <tomsabu0000@gmail.com>
2024-04-14 23:12:31 +0530

    major - sign up | in | dark theme | button

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/SideBar.css";
-import { useClerk } from "@clerk/clerk-react";
+import {
+  SignOutButton,
+  useClerk,
+  UserButton,
+  UserProfile,
+} from "@clerk/clerk-react";
 
 //
 import ManageAccountsSharpIcon from "@mui/icons-material/ManageAccountsSharp";
-
+import { Button } from "@mui/material";
 
 function SideBar() {
   const { user } = useClerk();
 
   const handleFullName = () => {
     return user.fullName.toUpperCase();
+  };
+
+  const [showProfile, setShowProfile] = useState(false);
+
+  const handleClick = () => {
+    setShowProfile(!showProfile);
   };
 
   return (
@@ -23,16 +34,28 @@ function SideBar() {
           <h2>{handleFullName()}</h2>
           <p>@{user.username}</p>
         </div>
-        <div className="account-icon">
+        <div className="account-icon" onClick={handleClick}>
           <ManageAccountsSharpIcon />
         </div>
+        {/* <div>{showProfile && <UserProfile />}</div> */}
       </div>
-      <br />
-      <hr />
       <br />
       <div className="follow-list">
         <h4>followers 0</h4>
         <h4>following 0</h4>
+      </div>
+      <br />
+      <hr />
+      <br />
+
+      <div className="nav-list">
+          <Button>Profile</Button>
+
+          <Button>Friend Request</Button>
+          {/* sign out */}
+          <SignOutButton>
+            <Button>logout</Button>
+          </SignOutButton>
       </div>
     </div>
   );
