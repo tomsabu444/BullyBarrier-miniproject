ce2d09d2c0788d7d3f39e6c7d96c8a841bbd1246
TOM SABU <tomsabu0000@gmail.com>
2024-04-20 20:36:53 +0530

    users post --section added

--- client/src/components/ShowPost.jsx
+++ client/src/components/ShowPost.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import { useClerk } from "@clerk/clerk-react";
+import React from "react";
+import "./style/ShowPost.css";
 function ShowPost() {
+  const { user } = useClerk();
+
   return (
-    <div>ShowPosdadsadadsadsadasdt</div>
-  )
+    <div className="users-posts">
+      <div className="users-info">
+        <div className="users-img-box">
+          <img draggable="false" src={user.imageUrl} alt="user-image" />
+        </div>
+        <div className="users-names">
+          <h3>testadssssssssssssss</h3>
+          <p>tomsabu444</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ShowPost
+export default ShowPost;
