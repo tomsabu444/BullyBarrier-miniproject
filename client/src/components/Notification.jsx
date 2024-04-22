a7c9d77a0f6d33c3fb6841e49c66bca91bb7adbc
TOM SABU <tomsabu0000@gmail.com>
2024-04-22 21:56:17 +0530

    Notification updated

---
+++ client/src/components/Notification.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style/Notify.css";
+import { Badge, Box } from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Notify() {
+  return (
+    <div className="notify">
+      <div className="nofity-title">
+        <h1>Notification</h1>
+        <div className="Nofity-icon">
+          <Badge badgeContent={2} color="warning">
+            <NotificationsIcon color="blue" />
+          </Badge>
+        </div>
+      </div>
+      <hr />
+
+      <div className="notification">
+      <Box component="section" sx={{ p: 2, border: '.5px solid' }}>
+          <h3>content</h3>
+        </Box>
+      </div>
+    </div>
+  );
+}
+
+export default Notify;
