3c8bc5894430a9bbd486b8c90ae04a89a93b6324
TOM SABU <tomsabu0000@gmail.com>
2024-04-27 19:32:54 +0530

    "Add notifications array and map through it to render each notification"

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -4,25 +4,35 @@ import { Badge, Box } from "@mui/material";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
 function Notify() {
+  //sample
+  const notifications = [
+    "Warning: Your recent message 'message' contains bullying content",
+  ];
+
   return (
     <div className="notify">
       <div className="nofity-title">
         <h1>Notification</h1>
         <div className="Nofity-icon">
-          <Badge badgeContent={2} color="warning">
+          {/* 
+          //! notification count  
+          */}
+          <Badge badgeContent={notifications.length} color="warning">
             <NotificationsIcon color="blue" />
           </Badge>
         </div>
       </div>
       <hr />
 
-      <div className="notification">
-        <Box sx={{ p: 1 }}>
-          <p>Warning: Your recent message "message" contains bullying content</p>
-        </Box>
-        <hr />
-        
-      </div>
+      {/* Map through the notifications array and render each notification */}
+      {notifications.map((notification, index) => (
+        <div key={index} className="notification">
+          <Box sx={{ p: 1 }}>
+            <p>{notification}</p>
+          </Box>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
