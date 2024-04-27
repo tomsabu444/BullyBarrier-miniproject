4b9ab0f2f664cef506bc3ef964352ffec2a54c59
TOM SABU <tomsabu0000@gmail.com>
2024-04-27 20:10:42 +0530

    Update ShowPost.jsx to display sample posts and add color; update ShowPost.css to style the sample posts

--- client/src/components/ShowPost.jsx
+++ client/src/components/ShowPost.jsx
@@ -1,231 +1,42 @@
-import { useClerk } from "@clerk/clerk-react";
 import React from "react";
+import { useClerk } from "@clerk/clerk-react";
 import "./style/ShowPost.css";
+
 function ShowPost() {
   const { user } = useClerk();
 
+  // Sample array of posts with color
+  const posts = [
+    { id: 1, content: "Lorem ipsum dolor sit amet 1", time: "10 min ago", color: "rgb(44, 252, 2)" },
+    { id: 2, content: "Lorem ipsum dolor sit amet 2", time: "20 min ago", color: "rgb(255, 0, 0)" },
+    { id: 3, content: "Lorem ipsum dolor sit amet 3", time: "30 min ago", color: "rgb(255, 0, 0)" },
+    // Add more posts as needed
+  ];
+
   return (
     <div className="feed">
-      {/* User  posts start */}
-      <div className="users-posts">
-        <div className="users-info">
-          <div className="users-img-box">
-            <img draggable="false" src={user.imageUrl} alt="user-image" />
-          </div>
-          <div className="posted-user">
-            <div className="users-names">
-              <h4>tom sabu </h4>
-              <p>tomsabu444 </p>
+      {posts.map((post) => (
+        <div key={post.id} className="users-posts">
+          <div className="users-info">
+            <div className="users-img-box">
+              <img draggable="false" src={user.imageUrl} alt="user-image" />
             </div>
-            <span>10 min ago</span>
-          </div>
-        </div>
-
-        <div className="comment-box">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            ex voluptatibus officiis eos quasi aspernatur distinctio
-            exercitationem cumque fugiat explicabo. Non, a. Quod, nihil
-            consequuntur. Praesentium perspiciatis repellat doloribus totam!
-          </p>
-        </div>
-        <div className="feed-hr-end">
-          <hr />
-        </div>
-      </div>
-      {/* User  posts end */}
-
-
-            {/* User  posts start */}
-            <div className="users-posts">
-        <div className="users-info">
-          <div className="users-img-box">
-            <img draggable="false" src={user.imageUrl} alt="user-image" />
-          </div>
-          <div className="posted-user">
-            <div className="users-names">
-              <h4>tom sabu </h4>
-              <p>tomsabu444 </p>
+            <div className="posted-user">
+              <div className="users-names">
+                <h4>tom sabu </h4>
+                <p>tomsabu444 </p>
+              </div>
+              <span>{post.time}</span>
             </div>
-            <span>10 min ago</span>
+          </div>
+          <div className="comment-box" style={{ color: post.color }}>
+            <p>{post.content}</p>
+          </div>
+          <div className="feed-hr-end">
+            <hr />
           </div>
         </div>
-
-        <div className="comment-box">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            ex voluptatibus officiis eos quasi aspernatur distinctio
-            exercitationem cumque fugiat explicabo. Non, a. Quod, nihil
-            consequuntur. Praesentium perspiciatis repellat doloribus totam!
-          </p>
-        </div>
-        <div className="feed-hr-end">
-          <hr />
-        </div>
-      </div>
-      {/* User  posts end */}      {/* User  posts start */}
-      <div className="users-posts">
-        <div className="users-info">
-          <div className="users-img-box">
-            <img draggable="false" src={user.imageUrl} alt="user-image" />
-          </div>
-          <div className="posted-user">
-            <div className="users-names">
-              <h4>tom sabu </h4>
-              <p>tomsabu444 </p>
-            </div>
-            <span>10 min ago</span>
-          </div>
-        </div>
-
-        <div className="comment-box">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            ex voluptatibus officiis eos quasi aspernatur distinctio
-            exercitationem cumque fugiat explicabo. Non, a. Quod, nihil
-            consequuntur. Praesentium perspiciatis repellat doloribus totam!
-          </p>
-        </div>
-        <div className="feed-hr-end">
-          <hr />
-        </div>
-      </div>
-      {/* User  posts end */}      {/* User  posts start */}
-      <div className="users-posts">
-        <div className="users-info">
-          <div className="users-img-box">
-            <img draggable="false" src={user.imageUrl} alt="user-image" />
-          </div>
-          <div className="posted-user">
-            <div className="users-names">
-              <h4>tom sabu </h4>
-              <p>tomsabu444 </p>
-            </div>
-            <span>10 min ago</span>
-          </div>
-        </div>
-
-        <div className="comment-box">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            ex voluptatibus officiis eos quasi aspernatur distinctio
-            exercitationem cumque fugiat explicabo. Non, a. Quod, nihil
-            consequuntur. Praesentium perspiciatis repellat doloribus totam!
-          </p>
-        </div>
-        <div className="feed-hr-end">
-          <hr />
-        </div>
-      </div>
-      {/* User  posts end */}      {/* User  posts start */}
-      <div className="users-posts">
-        <div className="users-info">
-          <div className="users-img-box">
-            <img draggable="false" src={user.imageUrl} alt="user-image" />
-          </div>
-          <div className="posted-user">
-            <div className="users-names">
-              <h4>tom sabu </h4>
-              <p>tomsabu444 </p>
-            </div>
-            <span>10 min ago</span>
-          </div>
-        </div>
-
-        <div className="comment-box">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            ex voluptatibus officiis eos quasi aspernatur distinctio
-            exercitationem cumque fugiat explicabo. Non, a. Quod, nihil
-            consequuntur. Praesentium perspiciatis repellat doloribus totam!
-          </p>
-        </div>
-        <div className="feed-hr-end">
-          <hr />
-        </div>
-      </div>
-      {/* User  posts end */}      {/* User  posts start */}
-      <div className="users-posts">
-        <div className="users-info">
-          <div className="users-img-box">
-            <img draggable="false" src={user.imageUrl} alt="user-image" />
-          </div>
-          <div className="posted-user">
-            <div className="users-names">
-              <h4>tom sabu </h4>
-              <p>tomsabu444 </p>
-            </div>
-            <span>10 min ago</span>
-          </div>
-        </div>
-
-        <div className="comment-box">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            ex voluptatibus officiis eos quasi aspernatur distinctio
-            exercitationem cumque fugiat explicabo. Non, a. Quod, nihil
-            consequuntur. Praesentium perspiciatis repellat doloribus totam!
-          </p>
-        </div>
-        <div className="feed-hr-end">
-          <hr />
-        </div>
-      </div>
-      {/* User  posts end */}      {/* User  posts start */}
-      <div className="users-posts">
-        <div className="users-info">
-          <div className="users-img-box">
-            <img draggable="false" src={user.imageUrl} alt="user-image" />
-          </div>
-          <div className="posted-user">
-            <div className="users-names">
-              <h4>tom sabu </h4>
-              <p>tomsabu444 </p>
-            </div>
-            <span>10 min ago</span>
-          </div>
-        </div>
-
-        <div className="comment-box">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            ex voluptatibus officiis eos quasi aspernatur distinctio
-            exercitationem cumque fugiat explicabo. Non, a. Quod, nihil
-            consequuntur. Praesentium perspiciatis repellat doloribus totam!
-          </p>
-        </div>
-        <div className="feed-hr-end">
-          <hr />
-        </div>
-      </div>
-      {/* User  posts end */}      {/* User  posts start */}
-      <div className="users-posts">
-        <div className="users-info">
-          <div className="users-img-box">
-            <img draggable="false" src={user.imageUrl} alt="user-image" />
-          </div>
-          <div className="posted-user">
-            <div className="users-names">
-              <h4>tom sabu </h4>
-              <p>tomsabu444 </p>
-            </div>
-            <span>10 min ago</span>
-          </div>
-        </div>
-
-        <div className="comment-box">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            ex voluptatibus officiis eos quasi aspernatur distinctio
-            exercitationem cumque fugiat explicabo. Non, a. Quod, nihil
-            consequuntur. Praesentium perspiciatis repellat doloribus totam!
-          </p>
-        </div>
-        <div className="feed-hr-end">
-          <hr />
-        </div>
-      </div>
-      {/* User  posts end */}
+      ))}
     </div>
   );
 }
