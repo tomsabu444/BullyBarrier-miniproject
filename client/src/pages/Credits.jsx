495eafe88d21f3dd044c8ddbbcb9e8cc1587e749
SamMathew007 <sammathew29032003@gmail.com>
2024-04-28 12:32:53 +0530

    Logo is changed

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -6,18 +6,18 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 
 const teamMembers = [
   {
-    name: "James Alexander",
+    name: "Jyo",
     role: "Full-Stack ",
     avatar: "https://cdn.discordapp.com/attachments/1194336632162222081/1200454655684124805/image.png?ex=662f0cc5&is=662dbb45&hm=6674fd7c3f0acb4e085ea30e8783e9d3e186ecf6f3ae54fc605efddea3b2b96e&",
     social: [
       { icon: <LinkedInIcon />, link: "#" },
-      { icon: <GitHubIcon />, link: "#" },
+      { icon: <GitHubIcon />, link: "https://github.com/23Jyo" },
     ],
   },
   {
-    name: "Lilia Williams",
-    role: "UX/UI Designer",
-    avatar: "assets/liliya.jpg",
+    name: "Tom Sabu",
+    role: "Backend Developer",
+    avatar: "https://cdn.discordapp.com/attachments/1206089168736751648/1234036379445624852/tomsabu.jpg?ex=662f4570&is=662df3f0&hm=112c023cd39a1fcc6e98c6883601b5dcc8bac3bc6089fab4ec3d45fd80eaf820&",
     social: [
       { icon: <LinkedInIcon />, link: "#" },
       { icon: <GitHubIcon />, link: "#" },
