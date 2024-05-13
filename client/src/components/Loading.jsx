8ff669bcfc92b120631b208469fc5bf33c26e10d
TOM SABU <tomsabu0000@gmail.com>
2024-05-13 23:44:27 +0530

    loading compoent

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style/Loading.css"
+function Loading() {
+  return (
+    <div className="loader-component">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loading;
