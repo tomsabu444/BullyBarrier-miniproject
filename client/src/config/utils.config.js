70a0d3f5d458a34946d418eefea4ded9321f15d2
TOM SABU <tomsabu0000@gmail.com>
2024-05-15 07:25:34 +0530

    personal avatar for Roshan Jacob in Credits.jsx

--- client/src/config/utils.config.js
+++ client/src/config/utils.config.js
@@ -1,5 +1,5 @@
-// const SERVER_BASE_URL="https://bullybarrier-miniproject.onrender.com"
+const SERVER_BASE_URL="https://bullybarrier-miniproject.onrender.com"
 
-const SERVER_BASE_URL="http://localhost:5273"
+// const SERVER_BASE_URL="http://localhost:5273"
 
 export { SERVER_BASE_URL }
